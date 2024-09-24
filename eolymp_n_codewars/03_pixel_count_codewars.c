f6f5a04cdafc02520d832eb76c15b76b5106c7e9
Solidarov <pavlo.nesteruk.work@gmail.com>
2024-09-24 16:37:35 +0300

    Optimize third task

--- eolymp_n_codewars/03_pixel_count_codewars.c
+++ eolymp_n_codewars/03_pixel_count_codewars.c
@@ -1,17 +1,20 @@
 #include <stddef.h>
-#include <stdio.h>  
+#include <stdio.h>
 
 size_t count_pixels(size_t k) {
 
-    long long int result = 0;
+    long long int num_of_pixels;
+    long long int big_circle = 3 * (k + 2) + k;
+    long long int small_circle = k * k;
 
-    if ((k == 1)||(k==2)){
-        result = k*k + 3*(k+2)+k;
+    if (k == 1){
+        num_of_pixels = small_circle + big_circle;
+      
     }else{
-        long long int n = (k-2) * (k-2);
-        result = (k*k -n) + 3*(k+2)+k;
+        long long int inner_hole = (k - 2) * (k - 2);
+        num_of_pixels = small_circle - inner_hole + big_circle;
     }
 
-    return result;
+    return num_of_pixels;
 
 }
