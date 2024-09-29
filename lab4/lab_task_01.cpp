2b56f286c602efbe75ddd355e94cb1233c335b1b
Solidarov <pavlo.nesteruk.work@gmail.com>
2024-09-29 17:38:04 +0300

    Complete first task

---
+++ lab4/lab_task_01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+
+int main(){
+    //system("chcp 1251 >> null"); // функція для кирилиці
+    SetConsoleCP(1251);   
+	SetConsoleOutputCP(1251);
+    SetConsoleOutputCP(CP_UTF8);
+
+    cout << "\t\t\tІм'я та прізвище\n";
+    cout << "\n";
+    cout << "\t\tПовне ім'я:" << "\tНестерук Павло Романович\n";
+    cout << "\n";
+    cout << "\t\t\tДомашня адреса:" << "\tСонячна, 3\n";
+    cout << "\n";
+    getch();
+    return 0;
+}
