8175d52a3c1fc9f61660910f5b05851d804d92bb
Solidarov <pavlo.nesteruk.work@gmail.com>
2024-10-04 21:28:58 +0300

    Complete task from table 3.1

---
+++ lab5/lab_task_3-1.cpp
@@ -0,0 +1,36 @@
+/*
+    Завдання для варіанту №14:
+        ( 2x+3    за x <= -6
+    Y = { x^3-1   за -6 < x < 3
+        ( x^2-1   за x >= 3
+*/
+
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main(){
+
+    float x, y;
+
+    // Введення даних
+    cout << "Enter x value: ";
+    cin >> x;
+
+    // Обчислення результату
+    if (x <= -6){
+        y = 2 * x + 3; // для x <= -6
+
+    }else if (x < 3){
+        y = x * x * x -1; // для -6 < x < 3
+    
+    }else{
+        y = x * x - 1; // для x >= 3
+    }
+
+    // Виведення результату
+    cout << "Value of Y is: " << y << endl;
+
+    system("pause");
+
+}
