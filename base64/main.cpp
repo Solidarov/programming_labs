1241d8ab03b8e56793ee9c589c2a5afebf13cd5d
Pavlo Nesteruk <pavlo.nesteruk.work@gmail.com>
2025-03-02 20:11:06 +0200

    Add my base64 decoding

--- base64/main.cpp
+++ base64/main.cpp
@@ -1,52 +1,62 @@
 #include <iostream>
 #include <string>
 #include <bitset>
+#include <bits/stdc++.h>
 #include "b64/encode.h"
 using namespace std;
 
 
 string base64_encoding(string, int);
+string base64_decoding(string, int);\
+
+// Таблиця символів для Base64
+static const char b64_table[] = {
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
+};
 
 int main(){
-    string msg;
+    string msg, result_msg;
 
     cout << "Enter the message: ";
     getline(cin, msg);
 
     int msg_len = msg.length();
 
-    string result = base64_encoding(msg, msg_len);
+    bool flag;
+    cout << "\nEncode(1) or Decode(0) message: ";
+    cin >> flag;
+
+    if (flag){
+        result_msg = base64_encoding(msg, msg_len);
     
-    cout << "Encoded message by my base64:\t " << result << endl;
+        cout << "\nEncoded message by my base64:\t " << result_msg << endl;
+    
+        // Використання бібліотеки libb64
+        base64::encoder encoder;
+        string encoded;
+        encoded.resize(msg.size() * 2);
+    
+        int encoded_size = encoder.encode(msg.data(), msg.size(), &encoded[0]);
+    
+        int final_size = encoder.encode_end(&encoded[encoded_size]);
+        encoded_size += final_size;
+    
+        encoded.resize(encoded_size);
+    
+        cout << "Encoded message by libb64:\t " << encoded << endl;
 
-    // Використання бібліотеки libb64
-    base64::encoder encoder;
-    std::string encoded;
-    encoded.resize(msg.size() * 2);
-
-    int encoded_size = encoder.encode(msg.data(), msg.size(), &encoded[0]);
-
-    int final_size = encoder.encode_end(&encoded[encoded_size]);
-    encoded_size += final_size;
-
-    encoded.resize(encoded_size);
-
-    std::cout << "Encoded message by libb64:\t " << encoded << std::endl;
-
+    }else{
+        result_msg = base64_decoding(msg, msg_len);
+        cout << "\nDecoded message by my base64: " << result_msg << endl;
+    }
     return 0;
 }
 
 string base64_encoding(string msg, int msg_len){
-    
-    // Таблиця символів для Base64
-    static const char encoding[] = {
-        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-        'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-        'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'
-    };
-
     
     // Перетворення кожного символу з таблиці ASCII до двійкового представлення
     // Кожне представлення додаємо до binary_msg
@@ -71,7 +81,7 @@ string base64_encoding(string msg, int msg_len){
     for (int i = 0; i < binary_msg.length(); i += 6){
         string one_char = binary_msg.substr(i, 6); 
         int index = stoi(one_char, nullptr, 2); // переводить рядок у якому бінарне число у ціле число
-        result += encoding[index];
+        result += b64_table[index];
     }
 
     // якщо довжина символів не виходить кратною 4, 
@@ -79,6 +89,45 @@ string base64_encoding(string msg, int msg_len){
     int divby4 = result.length() % 4;
     if (divby4 != 0){
         result.append((4-divby4), '=');
+    }
+
+    return result;
+}
+
+string base64_decoding(string d_msg, int msg_len){
+
+    // Видалення зайвих = в повідомленні
+    int erase = 0;
+    for (int i = 1; i <= 2; i++){
+        if (d_msg[msg_len-i] == '='){
+            erase += 1;
+        }
+    }
+    d_msg.erase(msg_len-erase);
+    msg_len -= erase;
+
+    // знаходження індексів у таблиці Base64
+    int base64_table_idx[msg_len];
+    for (int i = 0; i < msg_len; i++){
+        int idx = find(b64_table, b64_table + 64, d_msg[i]) - b64_table;
+        base64_table_idx[i] = idx;
+    }
+
+    //перетворення індексів з таблиці Base64 у біти
+    string binary_msg;
+    for (int i = 0; i < msg_len; i++){
+        bitset<6> binary_repr(base64_table_idx[i]);
+        binary_msg += binary_repr.to_string();
+    }
+
+    //ділимо рядок на 8бітні частини, які переводимо у цілий тип
+    //а потім цілі числа використовуємо як індекси до знаків ASCII
+    // та записуємо їх до стрічки 
+    string result;
+    for (int i = 0; i < binary_msg.length(); i += 8){
+        string one_char = binary_msg.substr(i, 8); 
+        int index = stoi(one_char, nullptr, 2);
+        result += char(index);
     }
 
     return result;
